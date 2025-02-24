027760325d56034d1508ee4113c6655fb1df6f84
anafarinha07 <a6808anafarinha@aes.edu.pt>
2025-02-24 11:51:33 +0000

    save

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        natureza: '/natureza.html',
+        retratos: '/retratos.html',
+        preto: '/preto.html',
+        animal: '/animal.html',
+        eventos: '/eventos.html',
+        aldeias: '/aldeias.html'
+      }
+    }
+  }
 });
