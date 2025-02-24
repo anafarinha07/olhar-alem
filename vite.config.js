f971451e84df56b9ad72f5d4f065454edb575a3b
anafarinha07 <a6808anafarinha@aes.edu.pt>
2025-02-24 11:53:30 +0000

    save

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/',
   build: {
     rollupOptions: {
       input: {
-        main: '/index.html',
-        natureza: '/natureza.html',
-        retratos: '/retratos.html',
-        preto: '/preto.html',
-        animal: '/animal.html',
-        eventos: '/eventos.html',
-        aldeias: '/aldeias.html'
+        main: resolve(__dirname, 'index.html'),
+        natureza: resolve(__dirname, 'natureza.html'),
+        retratos: resolve(__dirname, 'retratos.html'),
+        preto: resolve(__dirname, 'preto.html'),
+        animal: resolve(__dirname, 'animal.html'),
+        eventos: resolve(__dirname, 'eventos.html'),
+        aldeias: resolve(__dirname, 'aldeias.html')
       }
     }
   }
