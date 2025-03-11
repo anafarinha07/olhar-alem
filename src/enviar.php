2432fe0cb58441d7505f6551d694ff340152f38a
anafarinha07 <a6808anafarinha@aes.edu.pt>
2025-03-11 10:03:56 +0000

    e

--- src/enviar.php
+++ src/enviar.php
@@ -7,20 +7,32 @@ require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
+// Captura os dados do formulário
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$para = "fotografia.olharalem@gmail.com";
+$assunto = "Novo Agendamento de Sessão Fotográfica";
+$corpo = "=== Novo Agendamento de Sessão ===\n\n";
+$corpo .= "Nome: " . filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING) . "\n";
+$corpo .= "Email: " . $email . "\n";
+$corpo .= "Data Desejada: " . filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING) . "\n";
+$corpo .= "Mensagem: " . filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING) . "\n";
+
 $mail = new PHPMailer(true);
 try {
     // Configuração do servidor SMTP
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com'; 
     $mail->SMTPAuth = true;
-    $mail->Username = 'fotografia.olharalem@gmail.com'; // Substitua pelo seu e-mail
-    $mail->Password = '2025sertavalongo'; // Use uma senha de aplicativo do Gmail
+    $mail->Username = 'seuemail@gmail.com'; // Substitua pelo seu e-mail
+    $mail->Password = 'suasenha'; // Use uma senha de aplicativo do Gmail
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port = 465;
 
     // Configuração do remetente e destinatários
     $mail->setFrom('fotografia.olharalem@gmail.com', 'Fotografia Olhar Além');
-    $mail->addAddress($email); // Envia para a pessoa que agendou
+    if (!empty($email)) {
+        $mail->addAddress($email); // Envia para a pessoa que agendou
+    }
     $mail->addAddress($para);  // Envia para você
 
     // Conteúdo do e-mail
@@ -28,15 +40,13 @@ try {
     $mail->Subject = $assunto;
     $mail->Body = $corpo;
 
-    // Ativar depuração para diagnóstico
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Exibe erros detalhados
-    $mail->Debugoutput = 'html';
-
+    // Desativar depuração para evitar erros Forbidden
+    $mail->SMTPDebug = 0;
+    
     // Enviar e-mail
     $mail->send();
     echo "<script>alert('Agendamento enviado com sucesso!'); window.location.href = 'index.html';</script>";
 } catch (Exception $e) {
     echo "<script>alert('Erro ao enviar: {$mail->ErrorInfo}'); window.location.href = 'index.html';</script>";
 }
-
 ?> 
