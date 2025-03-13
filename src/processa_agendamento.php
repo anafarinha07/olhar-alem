e80b601e6e4af8e5e6056e87dcc5fcf543c8d96d
anafarinha07 <a6808anafarinha@aes.edu.pt>
2025-03-13 14:44:38 +0000

    j

--- src/processa_agendamento.php
+++ src/processa_agendamento.php
@@ -1,52 +1,43 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
-// Captura os dados do formulário
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $para = "fotografia.olharalem@gmail.com";
 $assunto = "Novo Agendamento de Sessão Fotográfica";
-$corpo = "=== Novo Agendamento de Sessão ===\n\n";
+$corpo = "=== Novo Agendamento ===\n\n";
 $corpo .= "Nome: " . filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING) . "\n";
 $corpo .= "Email: " . $email . "\n";
-$corpo .= "Data Desejada: " . filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING) . "\n";
+$corpo .= "Data: " . filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING) . "\n";
 $corpo .= "Mensagem: " . filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING) . "\n";
 
 $mail = new PHPMailer(true);
 try {
-    // Configuração do servidor SMTP
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com'; 
+    $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'fotografia.olharalem@gmail.com'; // Altere para o email correto
-    $mail->Password = 'sua_senha_de_aplicativo'; // Substitua pela senha de aplicativo do Gmail
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Alterado para STARTTLS
-    $mail->Port = 587; // Porta alterada para 587
+    $mail->Username = 'fotografia.olharalem@gmail.com';
+    $mail->Password = 'SUA_SENHA_DE_APLICATIVO'; // Gere uma senha de aplicativo no Gmail
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-    // Configuração do remetente e destinatários
     $mail->setFrom('fotografia.olharalem@gmail.com', 'Fotografia Olhar Além');
     if (!empty($email)) {
-        $mail->addAddress($email); // Envia para a pessoa que agendou
+        $mail->addAddress($email);
     }
-    $mail->addAddress($para);  // Envia para você
+    $mail->addAddress($para);
 
-    // Conteúdo do e-mail
     $mail->isHTML(false);
     $mail->Subject = $assunto;
     $mail->Body = $corpo;
 
-    // Desativar depuração para evitar erros Forbidden
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    
-    // Enviar e-mail
     $mail->send();
     echo "<script>alert('Agendamento enviado com sucesso!'); window.location.href = 'index.html';</script>";
 } catch (Exception $e) {
     echo "<script>alert('Erro ao enviar: {$mail->ErrorInfo}'); window.location.href = 'index.html';</script>";
 }
-?> 
+?>
